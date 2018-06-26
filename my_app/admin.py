750f72674987246ad78b0ace6237688c1f663b16
Igor Pedro L M Martins <igooruh@icloud.com>
2018-06-26 00:54:53 -0300

    Criando Models e Admin

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Address
 
 # Register your models here.
+
+admin.site.register (Address)
+
+
+
+
+
--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,64 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+# CRUD - Create Retrieve Update Delete
+
+
+STATES_CHOICES = (
+    ('AC', 'Acre'),
+    ('AL', 'Alagoas'),
+    ('AP', 'Amapá'),
+    ('AM', 'Amazonas'),
+    ('BA', 'Bahia'),
+    ('CE', 'Ceará'),
+    ('DF', 'Distrito Federal'),
+    ('ES', 'Espírito Santo'),
+    ('GO', 'Goiás'),
+    ('MA', 'Maranhão'),
+    ('MT', 'Mato Grosso'),
+    ('MS', 'Mato Grosso do Sul'),
+    ('MG', 'Minas Gerais'),
+    ('PA', 'Pará'),
+    ('PB', 'Paraíba'),
+    ('PR', 'Paraná'),
+    ('PE', 'Pernambuco'),
+    ('PI', 'Piauí'),
+    ('RJ', 'Rio de Janeiro'),
+    ('RN', 'Rio Grande do Norte'),
+    ('RS', 'Rio Grande do Sul'),
+    ('RO', 'Rondônia'),
+    ('RR', 'Roraima'),
+    ('SC', 'Santa Catarina'),
+    ('SP', 'São Paulo'),
+    ('SE', 'Sergipe'),
+    ('TO', 'Tocantins'),
+)
+
+
+class Address (models.Model):
+
+    address = models.CharField (max_length = 255)
+    address_complement = models.CharField (max_length = 255, null = True, blank = True)
+    city = models.CharField (max_length = 255)
+    state = models.CharField (max_length = 255, choices = STATES_CHOICES)
+    '''
+    state = models.CharField (max_length = 255, choices = (
+        ('MG', 'Minas Gerais'),
+        ('SP', 'São Paulo'),
+        ('DF', 'Distrito Federal'),
+        ('RJ', 'Rio de Janeiro')
+    ))
+    
+    '''
+    country = models.CharField (max_length = 255)
+    user = models.ForeignKey (User, on_delete = models.PROTECT)
+
+    class Meta:
+
+        verbose_name_plural = 'Adresses'
+
+
+    def __str__(self):
+            return '%s, %s, %s' % (self.address, self.city, self.country)
+
